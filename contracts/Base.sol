758bd6458687f1ac7828f717763322e21fd64341
Sam <sam@blackarcs.org>
2020-11-29 18:38:30 -0400

    Base contract signatures

---
+++ contracts/Base.sol
@@ -0,0 +1,21 @@
+pragma solidity >=0.6.0;
+
+import './Core.sol';
+
+contract Base is Core {
+    //Fields
+    //struct Cow = {
+    //    bool isMeat,
+    //    uint256 cowId
+   // }
+    //enum State
+    //mapping(uint256 => Cow) cowsForSale;
+
+    //Events:
+    // event Raised(uint256 cowId)
+    // event ForSale(uint256 cowId)
+    // event Sold(uint256 cowId)
+    // event Transported(uint256 cowId)
+    // event Butchered(uint256 cowId)
+    // event Eaten(string message)
+}
