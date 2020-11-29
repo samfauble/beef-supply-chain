4e51a6865e01226ae221bfaf26fdae2a6374c253
Sam <sam@blackarcs.org>
2020-11-29 18:44:37 -0400

    Butcher contract method signatures

---
+++ contracts/Butcher.sol
@@ -0,0 +1,14 @@
+pragma solidity >=0.6.0;
+
+contract Butcher {
+    //Fields
+    //address butcherAddress
+    //uint8 zipCode
+    //mapping (uint256 => Cow) meatForSale;
+
+
+    //Methods:
+    // function buyCow(uint256 cowId) public
+    // function butcherCow(uint256 cowId) public
+    // function sellMeat(uint256 cowId) public
+}
