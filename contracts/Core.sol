3ea4e451904ac2c555d8559e878a56e5f2e24fef
Sam <sam@blackarcs.org>
2020-11-29 18:31:31 -0400

    file setup, Core contract method signatures

---
+++ contracts/Core.sol
@@ -0,0 +1,13 @@
+pragma solidity >=0.6.0;
+
+contract Core {
+    //Fields
+    //contractAddress
+    //owner
+
+
+    //Methods:
+    // function upgrade(address newAddress) public returns (bool success)
+    // function ownable(address owner) public returns (address newOwner)
+    // function pause(uint256 cowId) public
+}
