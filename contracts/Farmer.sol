276d011f9eefb82f0fc0c00d0c024dd86ce2cfa3
Sam <sam@blackarcs.org>
2020-11-29 18:41:46 -0400

    Farmer contract method signatures

---
+++ contracts/Farmer.sol
@@ -0,0 +1,13 @@
+pragma solidity >=0.6.0;
+
+contract Farmer {
+    //Fields
+    //address farmerAddress
+    //uint8 zipCode
+
+
+    //Methods:
+    // function raiseCow(uint256 cowId) public
+    // function putCowUpForSale(uint256 cowId) public
+    // function transportCow(uint256 cowId) public
+}
